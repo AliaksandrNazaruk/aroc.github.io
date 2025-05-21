d738f9ddd2a0b2077721f18cf5344e859f13bcfb
AliaksandrNazaruk <46998499+AliaksandrNazaruk@users.noreply.github.com>
2025-05-21 15:08:52 +0200

    Delete configuration.py

--- configuration.py
+++
@@ -1,8 +0,0 @@
-xarm_manipulator_ip     = "192.168.1.220"
-symovo_car_ip           = "192.168.1.100"
-web_server_ip           = "192.168.1.10"
-igus_motor_ip           = "192.168.1.230"
-igus_ws_host            = "0.0.0.0"
-symovo_car_number       = "15"
-igus_motor_port         = 502
-igus_ws_port            = 8020
