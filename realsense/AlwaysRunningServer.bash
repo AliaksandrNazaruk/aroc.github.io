938038ca7a05cbb02e81bd096d9f0b9d3529f6c4
AliaksandrNazaruk <46998499+AliaksandrNazaruk@users.noreply.github.com>
2025-06-30 00:09:29 +0200

    Delete realsense directory

--- realsense/AlwaysRunningServer.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# this statement checks if there is an instance of the EtherSenseServer running
-if [[ ! `ps -eaf | grep "python home/boris/backup 06.03/realsense/server.py" | grep -v grep` ]]; then
-# if not, EtherSenseServer is started with the PYTHONPATH set due to cron not passing Env 
-    PYTHONPATH=$HOME/bin/python3 "/home/boris/backup 06.03/realsense/server.py"
-fi
